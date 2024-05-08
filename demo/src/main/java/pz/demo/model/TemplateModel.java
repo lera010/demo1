0f15487dcad9db53a8ff4c00ca05242994f23067
Лера <lerayudanova05@gmail.com>
2024-05-08 10:27:47 +0500

    Initial commit

---
+++ demo/src/main/java/pz/demo/model/TemplateModel.java
@@ -0,0 +1,25 @@
+package pz.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TemplateModel {
+    private String message;
+    private String time;
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public String getTime() {
+        return time;
+    }
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
